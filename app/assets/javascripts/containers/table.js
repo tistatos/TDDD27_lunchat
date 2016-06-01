715509ade70763de7e3eb9f57b07719647629b08
tistatos <tistatos@gmail.com>
2016-06-02 00:04:25 +0200

    tweaks to components

--- app/assets/javascripts/containers/table.js
+++ app/assets/javascripts/containers/table.js
@@ -1,7 +1,7 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
-import RestaurantListComponent from '../components/restaurantListComponent.js.jsx';
+import TableComponent from '../components/tableComponent.js.jsx';
 
 function mapStateToProps(state) {
   return {
@@ -14,7 +14,7 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators(actionCreators, dispatch);
 }
 
-const RestaurantList = connect(mapStateToProps, mapDispatchToProps)(RestaurantListComponent);
+const Table = connect(mapStateToProps, mapDispatchToProps)(TableComponent);
 
-export default RestaurantList;
+export default Table;
 
--- app/assets/javascripts/containers/tableList.js
+++ app/assets/javascripts/containers/tableList.js
@@ -1,7 +1,7 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
-import TableComponent from '../components/TableComponent.js.jsx';
+import TableListComponent from '../components/tableListComponent.js.jsx';
 
 function mapStateToProps(state) {
   return {
@@ -14,7 +14,7 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators(actionCreators, dispatch);
 }
 
-const Table = connect(mapStateToProps, mapDispatchToProps)(TableComponent);
+const TableList = connect(mapStateToProps, mapDispatchToProps)(TableListComponent);
 
-export default Table;
+export default TableList;
 
